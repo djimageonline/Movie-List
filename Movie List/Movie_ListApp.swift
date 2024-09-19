41b1e05136437335b90103de8b6a96bbd0c51780
Johnny Proano <jproano22@yahoo.com>
2024-09-19 00:17:59 -0400

    Initial Commit

---
+++ Movie List/Movie_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ListApp.swift
+//  Movie List
+//
+//  Created by Johnny Proano on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
